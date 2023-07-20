a337fccf812e0934eb1db2e546765ce25304efd6
Edgar Herrera <edgarh09lfc@gmail.com>
2023-07-19 21:22:18 -0700

    Add .env statement to server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const connectDB = require('./config/database')
 const mainRoutes = require('./routes/main')
 const eventRoutes = require('./routes/events')
 
-require('dotenv').config({path: './config/.env'})
+require('dotenv').config({path: './config/.env' || '.env'})
 
 // Passport config
 require('./config/passport')(passport)
